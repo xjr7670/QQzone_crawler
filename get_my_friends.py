5247b649fb221dd273e23035273c9016c399b529
Cavin <xjr30226@126.com>
2016-12-31 13:20:51 +0800

    Get All Friend's number first

---
+++ get_my_friends.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+import requests
+from time import sleep
+import util
+
+class Get_friends_number(object):
+    '''Use to get one's friends from their qzone's entry list'''
+
+    def __init__(self):
+
+        self.headers = util.headers
+        self.base_url = util.parse_friends_url()
+        util.check_path('friends')
+        print('Start to get friends list and save it for ./friends folder')
+
+
+    def get_friends(self):
+        key = True
+        position = 0
+        while key:
+            url = self.base_url + '&offset=' + str(position)
+            referer = 'http://qzs.qq.com/qzone/v8/pages/setting/visit_v8.html'
+            self.headers['Referer'] = referer
+
+            print("\tDealing with position\t%d." % position)
+            res = requests.get(url, headers=self.headers)
+            html = res.text
+            with open('friends/offset' + str(position) + '.json', 'w') as f:
+                f.write(html)
+
+            # check whether the friend list is over
+            # if that, the uinlist is void list
+            with open('friends/offset' + str(position) + '.json') as f2:
+                con = f2.read()
+            if '''"uinlist":[]''' in con:
+                print("Get friends Finish")
+                break
+
+            position += 50
+            sleep(5)
